28f6b644b316dbd4df06fce551015e5d776b18b0
upamune <info@serizawa.me>
2017-11-29 15:19:36 +0900

    Fix fish config

--- config.fish
+++ config.fish
@@ -1,6 +1,15 @@
 set -x GOPATH $HOME
 set -x PATH $GOPATH/bin $PATH
 
-alias e='emacs'
+alias e='emacsclient -c'
+alias E='emacsclient -nw'
 alias gs='git status'
 alias gc='git commit'
+
+if test -d $HOME/.rbenv/bin
+  set -x PATH $HOME/.rbenv/bin $PATH
+end
+
+if which rbenv > /dev/null
+  rbenv init - | source
+end
