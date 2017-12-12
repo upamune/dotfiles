a2629aed0e4a7e7026b776916d09df6064e327e9
Yu SERIZAWA (@upamune) <github@serizawa.me>
2017-12-12 10:45:01 +0900

    Update fish

--- config.fish
+++ config.fish
@@ -6,10 +6,23 @@ alias E='emacsclient -nw'
 alias gs='git status'
 alias gc='git commit'
 
+if which xsel > /dev/null ^&1
+  alias pbcopy='xsel --clipboard --input'
+end
+
+if which rbenv > /dev/null ^&1
+  rbenv init - | source
+end
+
 if test -d $HOME/.rbenv/bin
   set -x PATH $HOME/.rbenv/bin $PATH
 end
 
-if which rbenv > /dev/null
-  rbenv init - | source
+if test -d $HOME/n
+  set -x N_PREFIX $HOME/n
+  set -x PATH $N_PREFIX/bin $PATH
+end
+
+if test -d $HOME/miniconda3
+  set -x PATH $HOME/miniconda3/bin $PATH
 end
