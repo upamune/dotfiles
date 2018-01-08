5d904b16add5d68aa683b142c6b1b36171ac883f
Yu SERIZAWA (@upamune) <github@serizawa.me>
2018-01-08 13:20:06 +0900

    Update

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash -eu
+#!/bin/bash -eu
 
 if [[ ! -d $HOME/.tmux/plugins/tpm ]] ; then
   mkdir -p $HOME/.tmux/plugins/tpm && git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
